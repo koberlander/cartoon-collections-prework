7cd7c2b03418da936e92867acaaed19e42fb2c04
ko kristin <koberlander115@gmail.com>
2019-02-09 18:38:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,9 @@ end
 def find_the_cheese(cheddar_cheese)
   if cheddar_cheese.include?("cheddar" || "gouda" || "camembert") { puts "string"
   } else {
-    puts nil
+    return nil
   }
+end
   # the array below is here to help
   #cheese_types = ["cheddar", "gouda", "camembert"]
 end
