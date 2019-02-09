956a08e03e044376bfc549f66c449f1796763739
ko kristin <koberlander115@gmail.com>
2019-02-09 18:37:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,10 @@ def long_planeteer_calls(long_planeteer_calls)
 end
 
 def find_the_cheese(cheddar_cheese)
-  if cheddar_cheese.include?("cheddar" || "gouda" || "camembert")  {
-    
+  if cheddar_cheese.include?("cheddar" || "gouda" || "camembert") { puts "string"
+  } else {
+    puts nil
+  }
   # the array below is here to help
   #cheese_types = ["cheddar", "gouda", "camembert"]
 end
